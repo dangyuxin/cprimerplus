51de236fd5d55cc9f58c879037615e71136e5e09
mejomejo <1975815008@gmail.com>
2022-11-21 05:19:57 +0800

    11

---
+++ c/age.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a=18;
+    int b=a*365;
+    printf("age:%d\nday:%d",a,b);
+    
+}
--- c/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("党宇鑫\n");
-    printf("党\n宇鑫\n");
-    printf("党");
-    printf("宇鑫");
-   getchar();
-    return 0;
-}
